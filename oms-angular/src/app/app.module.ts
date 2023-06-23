60de6b896a2994d5b48961fa68b81a1cb33aafbc
Emmanuel <peradilla.emman08@gmail.com>
2023-06-23 14:00:38 +0800

    created components

--- oms-angular/src/app/app.module.ts
+++ oms-angular/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { CategoryComponent } from './category/category.component';
+import { ProductComponent } from './product/product.component';
+import { OrderComponent } from './order/order.component';
+import { OrderStatusComponent } from './order-status/order-status.component';
+import { UsersComponent } from './users/users.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    CategoryComponent,
+    ProductComponent,
+    OrderComponent,
+    OrderStatusComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule,
