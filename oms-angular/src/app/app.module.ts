4a2c11641df4cf42199b5f9895b560fbf4d99716
FloresEdward <119044984+FloresEdward@users.noreply.github.com>
2023-06-29 11:48:32 +0800

    Added the modules for templates

--- oms-angular/src/app/app.module.ts
+++ oms-angular/src/app/app.module.ts
@@ -3,26 +3,48 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
+// import { DashboardComponent } from './dashboard/dashboard.component';
 import { CategoryComponent } from './category/category.component';
 import { ProductComponent } from './product/product.component';
 import { OrderComponent } from './order/order.component';
 import { OrderStatusComponent } from './order-status/order-status.component';
 import { UsersComponent } from './users/users.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FullComponent } from './layouts/full/full.component';
+import { AppHeaderComponent } from './layouts/full/header/header.component';
+import { AppSidebarComponent } from './layouts/full/sidebar/sidebar.component';
+import { MaterialModule } from './shared/material-module';
+import { SharedModule } from './shared/shared.module';
+import { BestSellerComponent } from './best-seller/best-seller.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent,
+    // DashboardComponent,
     CategoryComponent,
     ProductComponent,
     OrderComponent,
     OrderStatusComponent,
-    UsersComponent
+    UsersComponent,
+    FullComponent,
+    AppHeaderComponent,
+    AppSidebarComponent,
+    BestSellerComponent,
+    HomeComponent,
+    // BestSellerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    FlexLayoutModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MaterialModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
