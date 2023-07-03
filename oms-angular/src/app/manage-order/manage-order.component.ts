655bfb9717377152cf2d94a41a49a2d49b2e0442
FloresEdward <119044984+FloresEdward@users.noreply.github.com>
2023-07-03 14:16:12 +0800

    Fixed the styles of order module and add the manage-order modules

--- oms-angular/src/app/manage-order/manage-order.component.ts
+++ oms-angular/src/app/manage-order/manage-order.component.ts
@@ -3,12 +3,12 @@ import { AfterViewInit, OnInit, Component } from '@angular/core';
 @Component({
   selector: 'app-order',
   templateUrl: './manage-order.component.html',
-  styleUrls: ['./manage-order.component.css']
+  styleUrls: ['./manage-order.component.scss']
 })
 export class ManageOrderComponent implements OnInit {
   
   displayedColumns: string[] = ['name', 'email', 'contactNumber', 'paymentMethod', 'total', 'view'];
-  dataSource: any;
+  dataSource: any[] = [];
   responseMessage: any;
 
   // ngAfterViewInit(): void { }
@@ -16,11 +16,15 @@ export class ManageOrderComponent implements OnInit {
   constructor(){}
 
   ngOnInit(): void {
-    
+    this.tableData();
   }
 
   tableData() {
-
+    this.dataSource = [
+      { name: 'John Doe', email: 'john.doe@example.com', contactNumber: '1234567890', paymentMethod: 'Credit Card', total: '$100' },
+      { name: 'Jane Smith', email: 'jane.smith@example.com', contactNumber: '9876543210', paymentMethod: 'PayPal', total: '$150' },
+      // Add more data objects as needed
+    ];
   }
 
   applyFilter() {
@@ -32,10 +36,14 @@ export class ManageOrderComponent implements OnInit {
   }
 
   handleDeleteAction(values:any) {
-    
+
   }
 
   downloadReportAction(values:any) {
+
+  }
+
+  deleteBill() {
 
   }
 }
