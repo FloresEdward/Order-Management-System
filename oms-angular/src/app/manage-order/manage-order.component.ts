509006b0797d792cb3ebf977ee63e32ac9e0d388
FloresEdward <119044984+FloresEdward@users.noreply.github.com>
2023-07-03 11:06:22 +0800

    Inprogress manage-order page

--- oms-angular/src/app/manage-order/manage-order.component.ts
+++ oms-angular/src/app/manage-order/manage-order.component.ts
@@ -1,13 +1,41 @@
-import { AfterViewInit, Component } from '@angular/core';
+import { AfterViewInit, OnInit, Component } from '@angular/core';
 
 @Component({
   selector: 'app-order',
   templateUrl: './manage-order.component.html',
   styleUrls: ['./manage-order.component.css']
 })
-export class ManageOrderComponent implements AfterViewInit {
-  ngAfterViewInit(): void { }
+export class ManageOrderComponent implements OnInit {
+  
+  displayedColumns: string[] = ['name', 'email', 'contactNumber', 'paymentMethod', 'total', 'view'];
+  dataSource: any;
+  responseMessage: any;
 
+  // ngAfterViewInit(): void { }
+  
   constructor(){}
 
+  ngOnInit(): void {
+    
+  }
+
+  tableData() {
+
+  }
+
+  applyFilter() {
+
+  }
+
+  handleViewAction(values: any) {
+
+  }
+
+  handleDeleteAction(values:any) {
+    
+  }
+
+  downloadReportAction(values:any) {
+
+  }
 }
