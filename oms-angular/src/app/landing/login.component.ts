70806dd18403fc638ce14fb6c26e02352b0f6b6e
troy9393 <troy_mikael_esguerra@dlsu.edu.ph>
2023-07-04 23:04:01 +0800

    Initial setup of backend and frontend integration

--- oms-angular/src/app/landing/login.component.ts
+++ oms-angular/src/app/landing/login.component.ts
@@ -1,27 +1,59 @@
 import { Component, OnInit } from '@angular/core';
-import { AuthService } from '../services/auth.service';
-import { ActivatedRoute, Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthenticationService } from '../services/authentication.service';
 
 @Component({
   selector: 'app-best-seller',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent implements OnInit{
-  private returnUrl: string;
-  constructor(private authService: AuthService,
-    private route: ActivatedRoute,
-    private router: Router) { 
-      this.returnUrl =
-      this.route.snapshot.queryParams["returnUrl"] || "/app/dashboard";
-    }
+export class LoginComponent {
+  loginForm: FormGroup;
 
-  ngOnInit(): void {
-
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthenticationService
+  ) {
+    this.loginForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+    });
   }
 
-  login() {
-    this.authService.login("jdoe", "pass123");
-    this.router.navigateByUrl(this.returnUrl);
+  ngOnInit(): void {}
+
+  public login(): void {
+    const credentials = {
+      /* construct the authentication request object */
+    };
+    this.authService.authenticate(credentials).subscribe(
+      (response) => {
+        // Handle successful authentication
+      },
+      (error) => {
+        // Handle authentication error
+      }
+    );
   }
+
+  // Other methods and component logic
 }
+
+// export class LoginComponent implements OnInit{
+//   private returnUrl: string;
+//   constructor(private authService: AuthService,
+//     private route: ActivatedRoute,
+//     private router: Router) {
+//       this.returnUrl =
+//       this.route.snapshot.queryParams["returnUrl"] || "/app/dashboard";
+//     }
+
+//   ngOnInit(): void {
+
+//   }
+
+//   login() {
+//     this.authService.login("jdoe", "pass123");
+//     this.router.navigateByUrl(this.returnUrl);
+//   }
+// }
