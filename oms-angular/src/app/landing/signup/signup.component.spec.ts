f68efb3938e3e3dd83ba080af8207ae5a1de4593
Emmanuel <peradilla.emman08@gmail.com>
2023-07-09 14:42:13 +0800

    Signup validation fixed

--- oms-angular/src/app/landing/signup/signup.component.spec.ts
+++ oms-angular/src/app/landing/signup/signup.component.spec.ts
@@ -1,21 +1,141 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { SignupComponent } from './signup.component';
+import { AuthenticationService } from 'src/app/services/authentication.service';
 
 describe('SignupComponent', () => {
   let component: SignupComponent;
   let fixture: ComponentFixture<SignupComponent>;
+  let authenticationService: AuthenticationService;
+  let router: Router;
+  let activatedRoute: ActivatedRoute;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [SignupComponent],
+      imports: [ReactiveFormsModule, HttpClientTestingModule],
+      providers: [
+        AuthenticationService,
+        { provide: Router, useClass: RouterStub },
+        { provide: ActivatedRoute, useClass: ActivatedRouteStub },
+      ],
+    }).compileComponents();
+  });
 
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [SignupComponent]
-    });
     fixture = TestBed.createComponent(SignupComponent);
     component = fixture.componentInstance;
+    authenticationService = TestBed.inject(AuthenticationService);
+    router = TestBed.inject(Router);
+    activatedRoute = TestBed.inject(ActivatedRoute);
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create the component', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should initialize the form with empty values', () => {
+    expect(component.signupForm.value).toEqual({
+      firstname: '',
+      lastname: '',
+      email: '',
+      password: '',
+      confirmPassword: '',
+    });
+  });
+
+  it('should mark form controls as touched when calling markFormControlsAsTouched', () => {
+    const formControls = component.signupForm.controls;
+    spyOn(formControls['firstname'], 'markAsTouched');
+    spyOn(formControls['lastname'], 'markAsTouched');
+    spyOn(formControls['email'], 'markAsTouched');
+    spyOn(formControls['password'], 'markAsTouched');
+    spyOn(formControls['confirmPassword'], 'markAsTouched');
+
+    component.markFormControlsAsTouched();
+
+    expect(formControls['firstname'].markAsTouched).toHaveBeenCalled();
+    expect(formControls['lastname'].markAsTouched).toHaveBeenCalled();
+    expect(formControls['email'].markAsTouched).toHaveBeenCalled();
+    expect(formControls['password'].markAsTouched).toHaveBeenCalled();
+    expect(formControls['confirmPassword'].markAsTouched).toHaveBeenCalled();
+  });
+
+  it('should return true when calling isFieldInvalid with an invalid field', () => {
+    const formControl = component.signupForm.controls['firstname'];
+    formControl.setErrors({ required: true });
+    formControl.markAsDirty();
+
+    const result = component.isFieldInvalid('firstname');
+
+    expect(result).toBeTrue();
+  });
+
+  it('should return false when calling isFieldInvalid with a valid field', () => {
+    const formControl = component.signupForm.controls['firstname'];
+    formControl.setErrors(null);
+    formControl.markAsDirty();
+
+    const result = component.isFieldInvalid('firstname');
+
+    expect(result).toBeFalse();
+  });
+
+  it('should return null when calling matchPasswordValidator with matching passwords', () => {
+    const password = 'password';
+    component.signupForm.controls['password'].setValue(password);
+    const formControl = { value: password };
+
+    const result = component.matchPasswordValidator()(formControl as any);
+
+    expect(result).toBeNull();
+  });
+
+  it('should return mismatch error when calling matchPasswordValidator with non-matching passwords', () => {
+    const password = 'password';
+    component.signupForm.controls['password'].setValue(password);
+    const formControl = { value: 'different-password' };
+
+    const result = component.matchPasswordValidator()(formControl as any);
+
+    expect(result).toEqual({ mismatch: true });
+  });
+
+  // it('should call authenticationService.register and navigate to returnUrl when calling signup', () => {
+  //   const userDetails = {
+  //     email: 'test@example.com',
+  //     password: 'password',
+  //     firstname: 'John',
+  //     lastname: 'Doe',
+  //   };
+  //   const returnUrl = '/dashboard';
+  //   spyOn(authenticationService, 'register').and.returnValue({
+  //     subscribe: (success: any) => {
+  //       success();
+  //     },
+  //   });
+  //   spyOn(router, 'navigateByUrl');
+
+  //   component.returnUrl = returnUrl;
+  //   component.signupForm.setValue(userDetails);
+  //   component.signup();
+
+  //   expect(authenticationService.register).toHaveBeenCalledWith(userDetails);
+  //   expect(router.navigateByUrl).toHaveBeenCalledWith(returnUrl);
+  // });
 });
+
+class RouterStub {
+  navigateByUrl(url: string): void {}
+}
+
+class ActivatedRouteStub {
+  snapshot = {
+    queryParams: {
+      returnUrl: '/dashboard',
+    },
+  };
+}
--- oms-angular/src/app/landing/signup/signup.component.ts
+++ oms-angular/src/app/landing/signup/signup.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AuthenticationService } from 'src/app/services/authentication.service';
 
@@ -9,6 +10,9 @@ import { AuthenticationService } from 'src/app/services/authentication.service';
   styleUrls: ['./signup.component.scss', '../landing.component.scss']
 })
 export class SignupComponent {
+
+  signupForm: FormGroup;
+  
   firstname: string = "";
   lastname: string = "";
   email: string = "";
@@ -16,11 +20,47 @@ export class SignupComponent {
   
   private returnUrl: string;
 
-  constructor(private authenticationService: AuthenticationService, private router: Router, private http: HttpClient, private route: ActivatedRoute){
+  constructor(private authenticationService: AuthenticationService, private router: Router, private http: HttpClient, private route: ActivatedRoute,
+    private formBuilder: FormBuilder){
     this.returnUrl = this.route.snapshot.queryParams["returnUrl"] || "/";
+
+    this.signupForm = this.formBuilder.group({
+      firstname: ['', Validators.required],
+      lastname: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8), Validators.pattern(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[^\w\s]).{8,}$/)]],
+      confirmPassword: ['', [Validators.required, this.matchPasswordValidator()]],
+    });
+  }
+
+  isFieldInvalid(field: string): boolean {
+    const control: any = this.signupForm.get(field);
+    return control.invalid && (control.dirty || control.touched);
+  }
+
+  markFormControlsAsTouched(): void {
+    Object.values(this.signupForm.controls).forEach(control => {
+      control.markAsTouched();
+    });
+  }
+
+  matchPasswordValidator() {
+    return (control: AbstractControl) => {
+      const password = this.signupForm?.get('password')?.value;
+      const confirmPassword = control.value;
+      return password === confirmPassword ? null : { mismatch: true };
+    };
   }
 
   public signup(): void {
+
+    if (this.signupForm.invalid) {
+      this.markFormControlsAsTouched();
+      return;
+    }
+
+    console.log(this.signupForm.value);
+    
     console.log(this.email);
     console.log(this.password);
 
@@ -30,7 +70,9 @@ export class SignupComponent {
       password: this.password,
       firstname: this.firstname,
       lastame: this.lastname
+      // Add default Role to this object before insert to database
     };
+    // check if email is unique
     this.authenticationService.register(userDetails).subscribe(
       (response) => {
         console.log(response)
