0ba94ee4c5583407461b6b6a32d6a067d6f2e3e7
Emmanuel <peradilla.emman08@gmail.com>
2023-07-14 11:56:30 +0800

    Order Status widget fixed

--- oms-angular/src/app/dashboard/dashboard.component.ts
+++ oms-angular/src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
 import { DatePipe, formatDate } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
-import { Component, Inject, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, Inject, OnInit, ViewChild } from '@angular/core';
+import { OrderStatusComponent } from './widgets/order-status.component';
 
 @Component({
   selector: 'app-dashboard',
@@ -11,11 +12,17 @@ import { Component, Inject, OnInit } from '@angular/core';
 export class DashboardComponent implements OnInit {
   selectedDate: Date;
   filteredData: any[] = [];
-  cardTitle = 'Dashboard';
 
+  // WIDGET orderStatus
+  orderStatusByDay: any[] = [];
+
+  cardTitle = 'Dashboard';
   orderItems: any[] = [];
 
-  constructor(private datePipe: DatePipe, private http: HttpClient) {
+  @ViewChild('orderStatusComponent', { static: false }) orderStatusComponent: OrderStatusComponent | undefined;
+
+
+  constructor(private datePipe: DatePipe, private http: HttpClient, private cdr: ChangeDetectorRef) {
     this.selectedDate = new Date();
   }
 
@@ -27,29 +34,36 @@ export class DashboardComponent implements OnInit {
     this.http.get<any[]>("http://localhost:8080/api/v1/management/order/getAll")
       .subscribe((resultData: any[]) => {
         this.orderItems = resultData;
+        this.onDateSelected();
       });
-      
-      
   }
-  
+
   onDateSelected(): void {
     const formattedDate = formatDate(this.selectedDate, 'yyyy-MM-dd', 'en-US');
     const startDate = new Date(formattedDate + 'T00:00:00.000+00:00');
     const endDate = new Date(formattedDate + 'T23:59:59.999+00:00');
 
-    const filteredOrders = this.filterOrdersByDate(this.orderItems, startDate, endDate);
-    console.log(filteredOrders);
-    
+    this.orderStatusByDay = this.filterOrdersByDate(this.orderItems, startDate, endDate);
+
+    // Update filteredData for other components if needed
+    this.filteredData = this.orderStatusByDay;
+
+    // Call a function to update the order status component
+    this.updateOrderStatusComponent();
+
   }
-  
+
   filterOrdersByDate(orders: any[], startDate: Date, endDate: Date): any[] {
     return orders.filter((order) => {
       const createdAt = new Date(order.createdAt);
       return createdAt >= startDate && createdAt <= endDate;
     });
   }
-  
-  
+
+  updateOrderStatusComponent(): void {
+    this.cdr.detectChanges();
+  }
+
   dateFilter = (date: Date | null): boolean => {
     const currentDate = new Date();
     return !date || date <= currentDate;
--- oms-angular/src/app/dashboard/widgets/order-status.component.ts
+++ oms-angular/src/app/dashboard/widgets/order-status.component.ts
@@ -1,5 +1,4 @@
-import { Component } from '@angular/core';
-import { Chart } from 'angular-highcharts';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'widget-order-status',
@@ -7,10 +6,32 @@ import { Chart } from 'angular-highcharts';
   styleUrls: ['./order-status.component.scss']
 })
 export class OrderStatusComponent {
+  @Input() orderStatusByDay: any[] = [];
 
-  chart = new Chart({
-    accessibility: {
-      enabled: false
-    },
-  });
+  cancel: number | undefined;
+  fulfilled: number | undefined;
+  pending: number | undefined;
+
+  ngOnInit() {
+    this.updateStatusCounts();
+  }
+
+  ngOnChanges() {
+    this.updateStatusCounts();
+  }
+
+  updateStatusCounts() {
+    const statusCounts = this.orderStatusByDay.reduce((counts, order) => {
+      const status = order.status;
+      counts[status] = (counts[status] || 0) + 1;
+      return counts;
+    }, {});
+
+    this.cancel = statusCounts['Cancelled'] || 0;
+    this.fulfilled = statusCounts['fulfilled'] || 0;
+    this.pending = statusCounts['pending'] || 0;
+    console.log(`Cancelled: ${this.cancel}`);
+    console.log(`Fulfilled: ${this.fulfilled}`);
+    console.log(`Pending: ${this.pending}`);
+  }
 }
