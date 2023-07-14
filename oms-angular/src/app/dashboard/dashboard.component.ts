d84d34b8f1d88cfc3838d9ee54775bed7e15adea
Emmanuel <peradilla.emman08@gmail.com>
2023-07-14 11:23:34 +0800

    Added Data filter per date

--- oms-angular/src/app/dashboard/dashboard.component.ts
+++ oms-angular/src/app/dashboard/dashboard.component.ts
@@ -1,11 +1,6 @@
-import { DatePipe } from '@angular/common';
+import { DatePipe, formatDate } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
-
-interface Order {
-  updateDate: string;
-  quantity: number;
-}
+import { Component, Inject, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
@@ -18,7 +13,7 @@ export class DashboardComponent implements OnInit {
   filteredData: any[] = [];
   cardTitle = 'Dashboard';
 
-  orderItems: Order[] = [];
+  orderItems: any[] = [];
 
   constructor(private datePipe: DatePipe, private http: HttpClient) {
     this.selectedDate = new Date();
@@ -29,12 +24,32 @@ export class DashboardComponent implements OnInit {
   }
 
   getAllOrder(): void {
-    this.http.get<Order[]>("http://localhost:8080/api/v1/management/order/getAll")
-      .subscribe((resultData: Order[]) => {
+    this.http.get<any[]>("http://localhost:8080/api/v1/management/order/getAll")
+      .subscribe((resultData: any[]) => {
         this.orderItems = resultData;
       });
+      
+      
   }
+  
+  onDateSelected(): void {
+    const formattedDate = formatDate(this.selectedDate, 'yyyy-MM-dd', 'en-US');
+    const startDate = new Date(formattedDate + 'T00:00:00.000+00:00');
+    const endDate = new Date(formattedDate + 'T23:59:59.999+00:00');
 
+    const filteredOrders = this.filterOrdersByDate(this.orderItems, startDate, endDate);
+    console.log(filteredOrders);
+    
+  }
+  
+  filterOrdersByDate(orders: any[], startDate: Date, endDate: Date): any[] {
+    return orders.filter((order) => {
+      const createdAt = new Date(order.createdAt);
+      return createdAt >= startDate && createdAt <= endDate;
+    });
+  }
+  
+  
   dateFilter = (date: Date | null): boolean => {
     const currentDate = new Date();
     return !date || date <= currentDate;
